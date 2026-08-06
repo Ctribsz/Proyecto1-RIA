405f8942261438873236bdad44e2ffed34371055
Ctribsz <ech221441@uvg.edu.gt>
2026-08-06 15:02:00 -0600

    feat(security): proteger la función holaMundo con la whitelist de IP

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,14 +1,21 @@
+import express from "express";
 import { logger } from "firebase-functions";
 import { onRequest } from "firebase-functions/v2/https";
 
 import { functionRegion } from "./config";
+import { ipWhitelist } from "./middleware/ipWhitelist";
+
+const helloApp = express();
+helloApp.disable("x-powered-by");
+helloApp.use(ipWhitelist);
+helloApp.get("/", (_request, response) => {
+  logger.info("La función holaMundo fue llamada.");
+  response.send(
+    `Hola Mundo desde Firebase Functions.\nFecha: ${new Date().toISOString()}`,
+  );
+});
 
 export const holaMundo = onRequest(
   { region: functionRegion, invoker: "public" },
-  (_request, response) => {
-    logger.info("La función holaMundo fue llamada.");
-    response.send(
-      `Hola Mundo desde Firebase Functions.\nFecha: ${new Date().toISOString()}`,
-    );
-  },
+  helloApp,
 );
